c4985806e96d49c64b8afb502043014d7bdf4351
Zhxade <141938142+Jeffrey-Uzoma@users.noreply.github.com>
2026-03-24 05:28:34 +0100

    Add files via upload

---
+++ client/src/utils/hero.ts
@@ -0,0 +1,13 @@
+import Earth from '../assets/Earth2.jpg';
+import Woman_dirt from '../assets/woman dirt.jpg'
+import About_waste from '../assets/About waste.jpg'
+import Earth_banner from '../assets/Earth banner.jpg'
+import Vinacorp_logo from "../assets/Vinacorp logo.jpg"
+
+export const HeroImages = {
+  Earth,
+  Woman_dirt,
+  About_waste,
+  Earth_banner,
+  Vinacorp_logo
+};
---
+++ client/src/utils/miniInfo.ts
@@ -0,0 +1,24 @@
+import Down_waste from '../assets/Down waste.jpg'
+import Number_one from '../assets/number one.jpg'
+import Number_two from '../assets/number two.jpg'
+
+export const MiniInfo = [
+  {
+    id: 1,
+    image: Down_waste,
+    title: `Safety and healthcare in the environment is very paramount`,
+    subText: `In the world of Today, waste and disposable is rampant around the world, and it's very important to curb all these as they pose risk to the well-being of life in general. Try keeping you cl....`
+  },
+  {
+    id: 2,
+    image: Number_one,
+    title: `Safety and healthcare in the environment is very paramount`,
+    subText: `In the world of Today, waste and disposable is rampant around the world, and it's very important to curb all these as they pose risk to the well-being of life in general. Try keeping you cl....`
+  },
+  {
+    id: 3,
+    image: Number_two,
+    title: `Safety and healthcare in the environment is very paramount`,
+    subText: `In the world of Today, waste and disposable is rampant around the world, and it's very important to curb all these as they pose risk to the well-being of life in general. Try keeping you cl....`
+  },
+]

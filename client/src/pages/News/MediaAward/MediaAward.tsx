f75431d9aca27d457052578ba875ef68feadf5a8
Zhxade <141938142+Jeffrey-Uzoma@users.noreply.github.com>
2026-03-24 05:26:59 +0100

    Add files via upload

---
+++ client/src/pages/News/MediaAward/MediaAward.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../../components/Navbar'
+
+const MediaAward: React.FC = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default MediaAward
